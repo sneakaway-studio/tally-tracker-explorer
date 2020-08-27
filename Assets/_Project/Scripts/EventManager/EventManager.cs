0a6172459f7e0dc633d78cc27de4d28e39d874cd
Owen Mundy <omundy@gmail.com>
2020-08-27 09:38:04 -0400

    Adds EventManager

---
+++ Assets/_Project/Scripts/EventManager/EventManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour
+{
+    // hold references to events
+    private Dictionary<string, UnityEvent> eventDictionary;
+
+    // singleton
+    private static EventManager eventManager;
+    public static EventManager instance
+    {
+        get
+        {
+            // if we don't have it
+            if (!eventManager)
+            {
+                // then find it 
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+                // if null
+                if (!eventManager)
+                {
+                    Debug.LogError("There needs to be one active EventManger script on a GameObject in your scene.");
+                }
+                else
+                {
+                    // initialize 
+                    eventManager.Init();
+                }
+            }
+            return eventManager;
+        }
+    }
+
+    void Init()
+    {
+        if (eventDictionary == null)
+        {
+            eventDictionary = new Dictionary<string, UnityEvent>();
+        }
+    }
+
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        // is there already a key/value pair?
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            // add new event
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+        // make sure not null
+        if (eventManager == null) return;
+        // otherwise remove event
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            // invoke event 
+            thisEvent.Invoke();
+        }
+    }
+
+}
