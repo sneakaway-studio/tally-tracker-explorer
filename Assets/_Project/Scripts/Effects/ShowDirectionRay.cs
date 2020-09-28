8eaaa199bff894fe3fb487e6ac446aef70c6c67d
Owen Mundy <omundy@gmail.com>
2020-09-28 16:50:54 -0400

    Updates animation timelines

---
+++ Assets/_Project/Scripts/Effects/ShowDirectionRay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDirectionRay : MonoBehaviour {
+
+    [ColorUsageAttribute (true, true)]
+    public Color color;
+
+    void Update ()
+    {
+        // Draw a ray pointing in the forward direction
+        Debug.DrawRay (transform.position, Vector3.forward * 10f, color);
+    }
+
+
+}
