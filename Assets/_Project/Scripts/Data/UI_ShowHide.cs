f8cfe4293742dc99f8d0efb000d18750e211f780
Owen Mundy <omundy@gmail.com>
2020-10-05 19:17:03 -0400

    Adds instructions to UI

---
+++ Assets/_Project/Scripts/Data/UI_ShowHide.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ShowHide : MonoBehaviour {
+
+
+    public GameObject debugParent;
+    public GameObject instructionsParent;
+
+
+
+    void Update ()
+    {
+        if (Input.GetKeyDown ("escape")) {
+            Application.Quit ();
+        } else if (Input.GetKeyDown (KeyCode.D)) {
+            ShowHide (debugParent);
+        } else if (Input.GetKeyDown (KeyCode.I)) {
+            ShowHide (instructionsParent);
+        }
+
+
+    }
+
+
+    void ShowHide (GameObject obj)
+    {
+        if (obj.activeSelf)
+            obj.SetActive (false);
+        else
+            obj.SetActive (true);
+
+    }
+
+
+
+
+}
