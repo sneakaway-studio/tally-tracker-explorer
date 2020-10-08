8244b999350a2163474aa3c717861d3969b475bf
Owen Mundy <omundy@gmail.com>
2020-10-08 11:38:51 -0400

    Hide test

--- Assets/_Project/Scripts/Players/Player.cs
+++ Assets/_Project/Scripts/Players/Player.cs
@@ -2,19 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
 
 
     public string username;
     public string avatarPath;
 
 
-    public void Init(string username, string avatarPath)
+    private void Awake ()
     {
+        StartCoroutine (StartChecks ());
+    }
+
+    public void Init (string username, string avatarPath)
+    {
+        Debug.Log ("Player.Init() username = " + username);
+
         this.username = username;
         this.avatarPath = avatarPath;
     }
+
+    /**
+     *  Hide if no username
+     */
+    IEnumerator StartChecks ()
+    {
+        yield return new WaitForSeconds (1f);
+
+        // you left your toys out
+        if (username == null || username == "")
+            gameObject.SetActive (false);
+    }
+
 
 
 }
